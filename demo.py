b800f4e09a23f5809efa9e1f7a080f5fbce3c74b
kubaunold <jakub@unold.pl>
2021-11-22 16:12:22 +0100

    Finished 1st tutorial - working cv cam.

--- demo.py
+++ demo.py
@@ -1,5 +1,18 @@
 from donkeycar.vehicle import Vehicle
+from cvcam import CvCam, CvImageDisplay
+
 
 V = Vehicle()
+
+
+
+
+cam = CvCam()
+V.add(cam, outputs=["camera/image"], threaded=True)
+
+disp = CvImageDisplay()
+V.add(disp, inputs=["camera/image"])
+
+
 
 V.start()
