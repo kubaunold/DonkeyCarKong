e34528d64412c2cb788cffd5263cfe629cdc6886
kubaunold <jakub@unold.pl>
2021-11-29 16:52:03 +0100

    Added new folder for test_videos

--- test_captureAndSaveVideo.py
+++ test_captureAndSaveVideo.py
@@ -1,5 +1,10 @@
+from datetime import datetime
 import cv2
 import imutils
+
+now = datetime.now()
+current_time = now.strftime("%d-%m-%y_%H:%M:%S")
+print("Current Time =", current_time)
 
 
 cap = cv2.VideoCapture(0)
@@ -7,7 +12,9 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
 size = (width, height)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('your_video.avi', fourcc, 20.0, size)
+# out = cv2.VideoWriter('your_video.avi', fourcc, 20.0, size)
+video_name = "test_videos/" + current_time + ".avi"
+out = cv2.VideoWriter(video_name, fourcc, 20.0, size)
 
 while(True):
     _, frame = cap.read()
